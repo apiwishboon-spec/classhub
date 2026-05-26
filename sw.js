68a070f074ff6edc6ecc7acf37fdf384b6626fd1
apiwish <apiwish.boon@gmail.com>
2026-05-26 19:35:00 +0700

    Force PWA update: bump SW cache from v3 to v4 with skipWaiting + clientsClaim, include all JS files

--- sw.js
+++ sw.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = 'classhub-v3';
+const CACHE_NAME = 'classhub-v4';
 const ASSETS = [
   './',
   './index.html',
+  './admin.html',
   './style.css',
   './script.js',
+  './admin.js',
+  './help.js',
+  './version.js',
+  './firebase-config.js',
+  './firebase-messaging-sw.js',
   './favicon.png',
+  './logo.png',
+  './manifest.json',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap',
   'https://unpkg.com/@phosphor-icons/web'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
