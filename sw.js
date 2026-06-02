5d3b3ef2d7485aa083c8d398104038ee878f18ee
apiwish <apiwish.boon@gmail.com>
2026-06-02 09:13:16 +0700

    feat: add missing service worker file

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'classhub-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/favicon.png',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
